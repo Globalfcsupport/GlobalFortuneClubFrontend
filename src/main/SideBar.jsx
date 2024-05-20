7bf2d4f7744fda81da89b2292de9443fc944675b
Fernick-Dass <fernickdass@gmail.com>
2024-05-20 18:59:46 +0530

    DashBoard

--- src/main/SideBar.jsx
+++ src/main/SideBar.jsx
@@ -27,30 +27,30 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         </div>
         {/* Sidebar Links */}
         <ul>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaTachometerAlt className="mr-3" /> DashBoard
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaWallet className="mr-3" /> Wallet
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaDice className="mr-3" /> FC Slots
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaUserFriends className="mr-3" /> Referrals
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaComments className="mr-3" /> Chat
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaLifeRing className="mr-3" /> Support
           </li>
-          <li className="py-2 px-4 border-b border-gray-200 flex items-center">
+          <li className="py-3 px-4 border-b border-gray-200 flex items-center">
             <FaCog className="mr-3" /> Settings
           </li>
         </ul>
         {/* Invite Section */}
-        <div className="mt-6 bg-blue-600 text-white text-center py-4 mx-4 rounded-lg">
+        <div className="mt-6 p-2 bg-blue-600 text-white text-center py-3 mx-4 rounded-lg">
           Invite your friends and earn $1 for every slot they create!
           <button className="mt-4 bg-white text-blue-600 px-4 py-2 rounded-full flex items-center mx-auto">
             <FaShareAlt className="mr-2" /> Share
