c5f3b8e04157b0186805f1757e77abe708c65fcd
Globalfcsupport <support@globalfc.app>
2024-06-12 16:49:26 +0530

    login page changes

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import OTPInput from 'react-otp-input';
-import Logo from '../../public/vite.svg';
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import Logo from '../assets/Image/logo-remove.png';
+import { loadCaptchaEnginge, validateCaptcha, LoadCanvasTemplateNoReload, LoadCanvasTemplate } from 'react-simple-captcha';
 import { Link, useNavigate } from 'react-router-dom';
 import { Login, sendOTP } from '../services/services';
 import { message, Button } from 'antd';
@@ -11,11 +11,9 @@ const FormikSignIn = () => {
 
   const [ sendOTPLoading, setSendOTPLoading ] = useState(false);
   const [ submitLoading, setSubmitLoading ] = useState(false);
-
-  const [ data, setData ] = useState({}); 
   const [ email, setEmail ] = useState('');
   const [captchaText, setCaptchaText] = useState('');
-  const [ showOTPInput, setOTPShowInput ] = useState(false);
+  const [ showOTPInput, setOTPShowInput ] = useState(true);
   const [ OTP, setOTP ] = useState('');
   const [messageApi, contextHolder] = message.useMessage();
   const navigate = useNavigate();
@@ -45,7 +43,38 @@ const FormikSignIn = () => {
     }
   }
 
-   const handleSubmit = (event) => {
+  const handleInput = (e)=> {
+    const target = e.target;
+    const val = target.value;
+ 
+    if (isNaN(val)) {
+        target.value = "";
+        return;
+    }
+ 
+    if (val != "") {
+        const next = target.nextElementSibling;
+        if (next) {
+            next.focus();
+        }
+    }
+  }
+
+  const handleBackSpace = (e)=> {
+    const target = e.target;
+    const key = e.key.toLowerCase();
+ 
+    if (key == "backspace" || key == "delete") {
+        target.value = "";
+        const prev = target.previousElementSibling;
+        if (prev) {
+            prev.focus();
+        }
+        return;
+    }
+  }
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitLoading(true);
     const user_captcha_value = document.getElementById('user_captcha_input').value;
@@ -81,66 +110,35 @@ const FormikSignIn = () => {
     <>
       {contextHolder}
     
-    <div  className="h-full px-5 gap-5 font-poppins flex flex-col justify-center" 
-      style={{
-        height: 'calc(100vh - 90px)',
-        scrollbarWidth: "none", // For Firefox
-        msOverflowStyle: "none" // For Internet Explorer and Edge
-      }}>
-      <div className='flex justify-center items-center flex-col gap-3'>
-        <img src={Logo} className='w-20' alt="Logo" />
-        <h1 className='text-xl font-bold text-blue-600'>Login</h1>
-        <h2 className='text-lg font-medium text-blue-600'>Welcome Back!</h2>
+    <div  className="h-[95vh] px-5 gap-5 font-poppins flex flex-col justify-center">
+      <div className='flex justify-center items-center flex-col gap-1'>
+        <img src={Logo} className='w-28' alt="Logo" />
+        <h2 className='text-xl font-medium text-blue-600'>Welcome Back!</h2>
+        <p className='text-xs'>Please Enter Your Email and OTP</p>
       </div>
       <div className='flex justify-center items-center w-full'>
         <form className="flex flex-col justify-center items-start w-full gap-5" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-2 relative w-full">
-            <label htmlFor='email' className='text-blue-800'>Email</label>
-            <input
-              required
-              type="email"
-              name="email"
-              placeholder='Enter Your Email'
-              className='w-full py-3 rounded-md pl-4 pr-32 outline-none text-sm'
-              onChange={(e)=> setEmail(e.target.value)}
-            />
-            <Button
-              // disabled={sendOTPLoading}
-              loading={sendOTPLoading}
-              className='absolute bg-blue-700 text-white px-5 py-1 rounded-md bottom-[0.375rem] right-1'
-              onClick={handleSendOTP}
-            >
+            <label htmlFor='email' className='font-semibold text-blue-800'>Email</label>
+            <input required type="email" name="email" placeholder='Enter Your Email' className='w-full py-3 rounded-md pl-4 pr-32 text-sm' onChange={(e)=> setEmail(e.target.value)} />
+            <Button loading={sendOTPLoading} className='absolute bg-blue-700 text-white px-5 py-1 rounded-md bottom-[0.375rem] h-9 right-1' onClick={handleSendOTP} >
               {sendOTPLoading ? 'Sending' : 'Send OTP'}
             </Button>
           </div>
           {showOTPInput && (
-            <div className='flex justify-center w-full'>
-              <OTPInput
-                name='OTP'
-                value={OTP}
-                onChange={(e)=>setOTP(e)}
-                numInputs={4}
-                renderInput={(props) => <input {...props} />}
-                inputType='number'
-                shouldAutoFocus
-                inputStyle={{
-                  width: '3rem',
-                  height: '3rem',
-                  margin: '0 0.5rem',
-                  fontSize: '1rem',
-                  borderRadius: '4px',
-                  border: '1px solid rgba(0,0,0,0.3)',
-                  textAlign: 'center',
-                  outline: 'none',
-                  fontFamily: 'poppins'
-                }}
-              />                                                                                                                                                                                                                                                                                                                        
+            <div className='flex flex-col gap-2 justify-center w-full'>
+              <label htmlFor='email' className='font-semibold text-blue-800 text-center'>OTP</label>
+              <div className='inputs flex justify-center gap-3'>
+                <input onKeyUp={handleBackSpace} onInput={handleInput} placeholder='' type='number' className='w-10 h-10 rounded-md'/>
+                <input onKeyUp={handleBackSpace} onInput={handleInput} placeholder='' type='number' className='w-10 h-10 rounded-md'/>
+                <input onKeyUp={handleBackSpace} onInput={handleInput} placeholder='' type='number' className='w-10 h-10 rounded-md'/>
+                <input onKeyUp={handleBackSpace} onInput={handleInput} placeholder='' type='number' className='w-10 h-10 rounded-md'/>
+              </div>                                                                                                                                                                                                                                                                                                                 
             </div>
           )}
-          <div className='flex flex-row gap-2 justify-center items-start w-full'>
-            <LoadCanvasTemplate />
+          <div className='flex gap-2 items-start w-full'>
+            <LoadCanvasTemplate reloadText='' />
             <input
-            //   type="text"
               id="user_captcha_input"
               name="user_captcha_input"
               placeholder='Enter Captcha'
