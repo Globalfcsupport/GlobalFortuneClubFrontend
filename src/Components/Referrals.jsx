3604b6bac36eb9314cc4d8118b2efbe703473771
MadhumithaWGS <madhumitha@whyglobalservices.com>
2024-07-19 14:46:52 +0530

    ReferralUI

--- src/Components/Referrals.jsx
+++ src/Components/Referrals.jsx
@@ -1,61 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { susers } from '../utils/Users';
 import { getUserByRefId } from '../services/services';
 import { BaseURL } from '../utils/const';
-import userIcon from "../assets/Image/user.png"
+import userIcon from "../assets/Image/user.png";
+
 const Referrals = () => {
   const [todayReferral, setTodayReferral] = useState(0);
   const [overallReferral, setOverallReferral] = useState(0);
-  const [ users, setUsers ] = useState([]);
-  const getRefDetails = async ()=>{
+  const [users, setUsers] = useState([]);
+
+  const getRefDetails = async () => {
     try {
-      let values = await getUserByRefId()
-      console.log(values.data,"ref");
-      setTodayReferral(values.data.todayCount)
-      setOverallReferral(values.data.overallCount)
-      setUsers(values.data.overallData)
+      let values = await getUserByRefId();
+      console.log(values.data, "ref");
+      setTodayReferral(values.data.todayCount);
+      setOverallReferral(values.data.overallCount);
+      setUsers(values.data.overallData);
     } catch (error) {
-      
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getRefDetails()
+    getRefDetails();
   }, []);
 
   return (
     <div className='h-full'>
-      <div className="flex flex-col bg-blue-500 p-5 gap-2">
-        <div className="relative ">
-          <input name='name' type="text" id='name' placeholder="Search ID/Name" className="w-full px-2 py-1 text-sm pr-10 border rounded-md outline-none" />
+      <div className="flex flex-col bg-primary p-3 gap-2">
+        <div className="relative">
+          <input
+            name='name'
+            type="text"
+            id='name'
+            placeholder="Search ID/Name"
+            className="w-full px-3 py-2 text-sm pr-10 border rounded-md outline-none"
+          />
           <button>
-            <FaSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 pointer-events-none" />
+            <FaSearch className="text-blueColor absolute right-3 top-1/2 transform -translate-y-1/2 text-MainSection pointer-events-none" />
           </button>
         </div>
 
-        <div className="flex justify-between">
-          <div className="flex flex-col gap-2 justify-center items-center ">
+        <div className="flex justify-between text-sm px-4">
+          <div className="flex flex-col gap-2 justify-center items-center">
             <p className='text-white'>Referral - Today</p>
             <p className="bg-white rounded-md w-16 text-center">{todayReferral}</p>
           </div>
-          <div className="flex flex-col gap-2 justify-center items-center ">
+          <div className="flex flex-col gap-2 justify-center items-center">
             <p className='text-white'>Referral - Overall</p>
             <p className="bg-white rounded-md w-16 text-center">{overallReferral}</p>
           </div>
         </div>
-
       </div>
-      <div className='w-full h-full bg-white flex flex-col overflow-scroll'>
-        { Array.isArray(users) && users.map((item, index)=> (
-          <div key={index} className='px-5 flex gap-5 p-2 items-center rounded-xl'>
-            <img src={item.image?`${BaseURL}${item.image}`:`${userIcon}`} className='h-10 w-10 rounded-full object-cover'/>
-            <div className='text-xs'>
-              <p>{item.userName}</p>
-              <p>Ref ID: {item.refId}</p>
+
+      <div className='w-full h-full bg-white flex flex-col overflow-scroll p-2'>
+        {Array.isArray(users) && users.map((item, index) => {
+          const firstLetter = item.userName ? item.userName.charAt(0).toUpperCase() : '';
+          return (
+            <div key={index} className='p-4 m-2 border rounded-xl shadow-md flex items-center bg-white'>
+              {item.image ? (
+                <img
+                  src={`${BaseURL}${item.image}`}
+                  className='h-12 w-12 rounded-full object-cover'
+                  alt="User"
+                />
+              ) : (
+                <div
+                  className='h-12 w-12 rounded-full flex items-center justify-center text-white'
+                  style={{ backgroundColor: 'rgb(158, 158, 158)' }}
+                >
+                  {firstLetter}
+                </div>
+              )}
+              <div className='ml-2 flex flex-col'>
+                <div className='flex justify-between items-center'>
+                  <p className='font-medium text-lg text-blueColor text-ellipsis line-clamp-1'>{item.userName}</p>
+                  <p className='text-blueColor text-xs font-bold'>ID: {item.refId}</p>
+                </div>
+                <p className='text-black-100 text-xs '>Email: {item.email}</p>
+                <p className='text-black-100 text-xs '>DOJ: {new Date(item.createdAt).toLocaleString()}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
